b0093b26e001c8e7a91e1c318fbddbe960e9a570
Mlziste.cz <kjuvideo@email.cz>
2026-06-29 10:40:01 +0000

    File changes

--- src/pages/Kolekce.jsx
+++ src/pages/Kolekce.jsx
@@ -113,16 +113,12 @@ export default function Kolekce() {
       base44.entities.Product.list().catch(() => []),
       base44.entities.ProductCategory.list().catch(() => []),
     ]).then(([prods, cats]) => {
-      // Enrich products with category name
       const enriched = (prods || []).map(p => ({
         ...p,
         _categoryName: (cats || []).find(c => c.id === p.category_id)?.name || '',
       }));
       setProducts(enriched);
       setCategories(cats || []);
-    }).catch(() => {
-      setProducts([]);
-      setCategories([]);
     }).finally(() => setLoading(false));
   }, []);
 

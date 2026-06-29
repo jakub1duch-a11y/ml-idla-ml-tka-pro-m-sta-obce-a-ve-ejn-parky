9b15d8f9e4ffaf698be50c38669b5ef37f2eacda
Mlziste.cz <kjuvideo@email.cz>
2026-06-29 22:10:44 +0000

    Visual edits

--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -178,7 +178,7 @@ export default function Kontakt() {
                     </datalist>
                   </div>
                   <div>
-                    <label className="text-xs font-mono text-white/40 tracking-widest uppercase block mb-1">
+                    <label className="text-xs font-mono text-white/40 tracking-widest uppercase block mb-1 hidden">
                       {form.request_type === 'volume_price' ? 'Počet kusů / kombinace' : 'Počet / rozsah'}
                     </label>
                     <input type="text"

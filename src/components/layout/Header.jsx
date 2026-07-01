c3759494f7a71d65f4c96aa053d49d4db0e4eb13
Mlziste.cz <kjuvideo@email.cz>
2026-07-01 20:11:03 +0000

    Visual edits

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -68,7 +68,7 @@ export default function Header() {
 
       
 
-      <header className={`fixed top-0 left-0 right-0 transition-all z-40 bg-black/60 backdrop-blur/0 duration-800 ${
+      <header className={`fixed top-0 left-0 right-0 transition-all z-40 bg-black/60 duration-800 backdrop-blur ${
       scrolled ? "border-slate-200 shadow-sm" : ""}`
       }>
         <div className="flex items-center justify-between max-w-7xl h-16 px-6 lg:px-8 mx-auto gap-4 lg:gap-8">

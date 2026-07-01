e09b6481f511af396e1650309dc84118eb339eb4
Mlziste.cz <kjuvideo@email.cz>
2026-07-01 15:57:26 +0000

    Visual edits

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -205,7 +205,7 @@ export default function HeroSection() {
         </AnimatePresence>
 
         {/* Bottom row: nav arrows + stats */}
-        <div className="flex items-end gap-6 mt-1">
+        <div className="flex items-end gap-6 mt-">
 
           {/* Prev/Next */}
           

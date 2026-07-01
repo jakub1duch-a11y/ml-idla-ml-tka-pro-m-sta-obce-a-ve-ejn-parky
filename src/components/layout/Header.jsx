bce80ee8e294fd352dfe23962da9125f6817b133
Mlziste.cz <kjuvideo@email.cz>
2026-07-01 15:40:18 +0000

    Visual edits

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -79,8 +79,8 @@ export default function Header() {
               <path d="M11 2 C11 2 2 12 2 18 C2 23.5 6 26.5 11 26.5 C16 26.5 20 23.5 20 18 C20 12 11 2 11 2Z" fill="#0f172a" fillOpacity="0.92" />
               <path d="M7 19 C7 21.5 8.8 23 11 23" stroke="white" strokeWidth="1.3" strokeLinecap="round" opacity="0.6" />
             </svg>
-            <span style={{ fontFamily: "'DM Sans', sans-serif", fontWeight: 300, letterSpacing: '-0.04em' }} className="leading-none text-3xl no-underline not-italic font-light [font-family:'Urbanist',_sans-serif] text-left normal-case text-slate-400">
-              mlzidla<span className="font-medium text-[#214999]">.cz</span>
+            <span style={{ fontFamily: "'DM Sans', sans-serif", fontWeight: 300, letterSpacing: '-0.04em' }} className="text-3xl no-underline not-italic font-light [font-family:'Urbanist',_sans-serif] text-left normal-case leading-non text-slate-700">
+              mlzidla<span className="text-[#214999] [font-family:'Urbanist',_sans-serif] text-xl font-light">.cz</span>
             </span>
           </Link>
 

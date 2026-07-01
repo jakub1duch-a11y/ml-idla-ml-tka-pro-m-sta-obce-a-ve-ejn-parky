5918abda5443ad6daceb26ef220bf99b3381da19
Mlziste.cz <kjuvideo@email.cz>
2026-07-01 15:36:47 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,12 @@ import React, { useEffect } from 'react';
 import { setSEO, SEO_PAGES, injectOrgJsonLd } from '@/lib/seo';
 import HeroSection from '@/components/home/HeroSection';
 import CategoriesSection from '@/components/home/CategoriesSection';
-import ProductsSection from '@/components/home/ProductsSection';
 import SmartSection from '@/components/home/SmartSection';
 import VideoSection from '@/components/home/VideoSection';
 import ProjectsSection from '@/components/home/ProjectsSection';
 import ReferenceSection from '@/components/home/ReferenceSection';
 import TechSection from '@/components/home/TechSection';
 import BlogSection from '@/components/home/BlogSection';
-import PromoSection from '@/components/home/PromoSection';
 import FeaturedProductsSection from '@/components/home/FeaturedProductsSection';
 import HowItWorksSection from '@/components/home/HowItWorksSection';
 import ContactSection from '@/components/home/ContactSection';
@@ -24,7 +22,6 @@ export default function Home() {
       <HeroSection />
       <CategoriesSection />
       <FeaturedProductsSection />
-      <ProductsSection />
       <HowItWorksSection />
       <SmartSection />
       <VideoSection />
@@ -32,7 +29,6 @@ export default function Home() {
       <ReferenceSection />
       <TechSection />
       <BlogSection />
-      <PromoSection />
       <ContactSection />
     </>
   );

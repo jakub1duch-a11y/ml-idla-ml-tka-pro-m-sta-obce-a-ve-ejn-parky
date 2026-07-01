a37569e29bf154b3317ae4c30808d61926070287
Mlziste.cz <kjuvideo@email.cz>
2026-07-01 16:15:19 +0000

    Visual edits

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -161,7 +161,7 @@ export default function HeroSection() {
       </div>
 
       {/* Main content */}
-      <div className="relative flex flex-col justify-end w-full z-5 max-w-7xl lg:px-8 mx-auto flex- px-6 pt-36 pb-1">
+      <div className="relative flex flex-col justify-end w-full z-5 max-w-7xl lg:px-8 mx-auto px-6 pt-36 pb-10">
 
         <AnimatePresence mode="wait" custom={direction}>
           <motion.div

152a68f0fb3564a3aa0e09c832d99cf2aaee2426
Mlziste.cz <kjuvideo@email.cz>
2026-07-01 15:45:21 +0000

    Visual edits

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -223,7 +223,7 @@ export default function HeroSection() {
           {/* Stats */}
           <div className="grid grid-cols-4 gap-2 lg:w-auto ml-auto">
             {stats.map((s) =>
-            <div key={s.val} className="bg-white/8 backdrop-blur-sm border py-3 text-center px-20 border-white/30 rounded-xl">
+            <div key={s.val} className="bg-white/8 backdrop-blur-sm border py-3 text-center px-20 border-white/30 rounded-1xl">
                 <p className="font-heading font-light text-xl text-white leading-none mb-1">{s.val}</p>
                 <p className="text-[10px] text-white/50 leading-tight">{s.label}</p>
               </div>

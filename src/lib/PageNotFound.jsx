9c606ef123a0ff2dacbb0aacb17c607be7decfd6
Mlziste.cz <kjuvideo@email.cz>
2026-07-07 11:54:23 +0000

    Manual code change

--- src/lib/PageNotFound.jsx
+++ src/lib/PageNotFound.jsx
@@ -71,5 +71,8 @@ export default function PageNotFound({}) {
                 </div>
             </div>
         </div>
+                     <span style={{ fontFamily: "'DM Sans', sans-serif", fontWeight: 300, letterSpacing: '0.05em' }} className={`no-underline not-italic [font-family:'Urbanist',_sans-serif] text-left leading-none text-2xl font-bold transition-colors duration-500 uppercase ${scrolled ? 'text-slate-900' : 'text-white'}`}>
+                mlžidla<span className="[font-family:'Urbanist',_sans-serif] text-xl normal-case px-1 font-medium text-[#40a2d4]" style={{ letterSpacing: '0.06em' }}>.cz</span>
+              </span>
     )
 }

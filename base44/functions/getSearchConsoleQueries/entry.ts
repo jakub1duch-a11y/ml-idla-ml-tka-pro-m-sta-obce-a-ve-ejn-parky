19ca932a87e6e1ebfdc7d55c51cdc36518459076
Mlziste.cz <kjuvideo@email.cz>
2026-07-08 10:44:20 +0000

    Manual code change

--- base44/functions/getSearchConsoleQueries/entry.ts
+++ base44/functions/getSearchConsoleQueries/entry.ts
@@ -1,6 +1,6 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.31';
 
-const SITE_URL = 'sc-domain:holmtec.cz';
+const SITE_URL = 'sc-domain:mlzidla.cz';
 
 Deno.serve(async (req) => {
   try {

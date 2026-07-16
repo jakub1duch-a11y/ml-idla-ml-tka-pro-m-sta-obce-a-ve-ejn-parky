b208055bf30c310a83945594f895046e3c4c4c56
Mlžiste.cz <kjuvideo@email.cz>
2026-07-16 08:24:56 +0000

    File changes

---
+++ src/lib/benefitNavigation.js
@@ -0,0 +1,6 @@
+export const BENEFIT_NAVIGATION = {
+  'zvyseni-trzeb-a-prodeje': 'Zvýšení tržeb a prodeje',
+  'automatizace-provozu': 'Automatizace provozu',
+  'zabezpeceni-a-shoda': 'Bezpečnost a shoda',
+  'snizovani-provoznich-nakladu': 'Snížení provozních nákladů'
+};
